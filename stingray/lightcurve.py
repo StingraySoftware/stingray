a03a85b3a6fd0f16fecec3e667c680f6347b78d5
andresgur <andress.gurpide@gmail.com>
2025-04-03 23:19:16 +0100

    Fixed valid_statistics, changed 'None' to 'none' in Lightcurve

--- stingray/lightcurve.py
+++ stingray/lightcurve.py
@@ -39,7 +39,7 @@ from stingray.loggingconfig import setup_logger
 
 __all__ = ["Lightcurve"]
 
-valid_statistics = ["poisson", "gauss", "None"]
+valid_statistics = ["poisson", "gauss", "none"]
 
 logger = setup_logger()
 
