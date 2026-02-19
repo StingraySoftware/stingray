Fix some corner case in error calculation and settings for low coherence

stingray/fourier.py
@@ -765,11 +765,17 @@ def raw_coherence(
     coherence = num / den
     min_uncertainty = 1 / n_ave
     uncertainty = (2**0.5 * coherence * (1 - coherence)) / (np.sqrt(coherence) * n_ave**0.5)
-    bad = np.where((coherence == 0) | (uncertainty < min_uncertainty))
-    if not isinstance(min_uncertainty, Iterable):
-        uncertainty[bad] = min_uncertainty
+    min_uncertainty = 1 / n_ave
+    if isinstance(coherence, Iterable):
+        bad = np.where((coherence == 0) | (uncertainty < min_uncertainty))
+        if not isinstance(min_uncertainty, Iterable):
+            uncertainty[bad] = min_uncertainty
+        else:
+            uncertainty[bad] = min_uncertainty[bad]
     else:
-        uncertainty[bad] = min_uncertainty[bad]
+        bad = (coherence == 0) | (uncertainty < min_uncertainty)
+        if bad:
+            uncertainty = min_uncertainty
 
     if return_uncertainty:
         return coherence, uncertainty
@@ -855,11 +861,16 @@ def intrinsic_coherence(
     uncertainty = coherence / np.sqrt(n_ave) * np.sqrt(err1 + err2 + err3)
 
     min_uncertainty = 1 / n_ave
-    bad = np.where((coherence == 0) | (uncertainty < min_uncertainty))
-    if not isinstance(min_uncertainty, Iterable):
-        uncertainty[bad] = min_uncertainty
+    if isinstance(coherence, Iterable):
+        bad = np.where((coherence == 0) | (uncertainty < min_uncertainty))
+        if not isinstance(min_uncertainty, Iterable):
+            uncertainty[bad] = min_uncertainty
+        else:
+            uncertainty[bad] = min_uncertainty[bad]
     else:
-        uncertainty[bad] = min_uncertainty[bad]
+        bad = (coherence == 0) | (uncertainty < min_uncertainty)
+        if bad:
+            uncertainty = min_uncertainty
 
     if return_uncertainty:
         return coherence, uncertainty

stingray/tests/test_fourier.py
@@ -204,15 +204,9 @@ class TestCoherence(object):
         P1noise = 495955
         P2noise = 494967
         coh = raw_coherence(C, P1, P2, P1noise, P2noise, 499, 1)
-
-        # The warning is only raised when one gives a single value for power.
-        with pytest.warns(
-            UserWarning,
-            match="Negative numerator in raw_coherence calculation. Setting bias term to 0",
-        ):
-            coh_sngl = raw_coherence(C[0], P1[0], P2[0], P1noise, P2noise, 499, 1)
-        assert np.allclose(coh, (C * np.conj(C)).real / (P1 * P2))
-        assert np.isclose(coh_sngl, (C * np.conj(C)).real[0] / (P1[0] * P2[0]))
+        coh_sngl = raw_coherence(C[0], P1[0], P2[0], P1noise, P2noise, 499, 1)
+        assert np.allclose(coh, 0)
+        assert np.isclose(coh_sngl, 0)
 
 
 class TestFourier(object):